5dd34f83f3d3fe0db1817c246329f360d1d9b1a3
vijoy-sunil <vijoy.sunil@gmail.com>
2025-01-18 21:39:32 +0530

    updated core enums

---
+++ App/Config/VKEnum.h
@@ -0,0 +1,40 @@
+#pragma once
+
+namespace Core {
+    typedef enum {
+        LAYER_LOG                       = 0,
+
+        LAYER_MODEL                     = 1,
+        LAYER_LIGHT                     = 2,
+        LAYER_CAMERA                    = 3,
+
+        LAYER_INSTANCE                  = 4,
+        LAYER_WINDOW                    = 5,
+        LAYER_SURFACE                   = 6,
+        LAYER_PHY_DEVICE                = 7,
+        LAYER_LOG_DEVICE                = 8,
+        LAYER_SWAP_CHAIN                = 9,
+
+        LAYER_STAGING_BUFFER            = 10,
+        LAYER_VERTEX_BUFFER             = 11,
+        LAYER_INDEX_BUFFER              = 12,
+        LAYER_UNIFORM_BUFFER            = 13,
+        LAYER_STORAGE_BUFFER            = 14,
+
+        LAYER_COLOR_IMAGE               = 15,
+        LAYER_DEPTH_IMAGE               = 16,
+        LAYER_SWAP_CHAIN_IMAGE          = 17,
+        LAYER_TEXTURE_IMAGE             = 18,
+
+        LAYER_SAMPLER                   = 19,
+
+        LAYER_RENDER_PASS               = 20,
+        LAYER_FRAME_BUFFER              = 21,
+        LAYER_PIPELINE                  = 22,
+        LAYER_DESCRIPTOR_POOL           = 23,
+        LAYER_DESCRIPTOR_SET_DYNAMIC    = 24,
+        LAYER_DESCRIPTOR_SET_STATIC     = 25,
+
+        LAYER_SYNC_PRIMITIVE            = 26
+    } e_layerType;
+}   // namespace Core
