bf5eade5916e6e8878aedd5094f51ac95d9d2482
vijoy-sunil <vijoy.sunil@gmail.com>
2025-01-24 18:04:21 +0530

    added fence,semaphore

---
+++ Core/VKFence.h
@@ -0,0 +1,116 @@
+#pragma once
+#define GLFW_INCLUDE_VULKAN
+#include <GLFW/glfw3.h>
+#include <vulkan/vk_enum_string_helper.h>
+#include "../Backend/Layer/LYInstanceBase.h"
+#include "../Backend/Log/LGImpl.h"
+#include "VKLogDevice.h"
+
+namespace Core {
+    class VKFence: public Layer::LYInstanceBase {
+        private:
+            struct FenceInfo {
+                struct Meta {
+                    VkFenceCreateFlags createFlags;
+                } meta;
+
+                struct State {
+                    bool logObjCreated;
+                } state;
+
+                struct Resource {
+                    Log::LGImpl* logObj;
+                    VKLogDevice* logDeviceObj;
+                    VkFence fence;
+                } resource;
+            } m_fenceInfo;
+
+        public:
+            VKFence (Log::LGImpl* logObj,
+                     VKLogDevice* logDeviceObj) {
+
+                m_fenceInfo = {};
+
+                if (logObj == nullptr) {
+                    m_fenceInfo.resource.logObj     = new Log::LGImpl();
+                    m_fenceInfo.state.logObjCreated = true;
+
+                    m_fenceInfo.resource.logObj->initLogInfo ("Build/Log/Core", __FILE__);
+                    LOG_WARNING (m_fenceInfo.resource.logObj) << NULL_LOGOBJ_MSG
+                                                              << std::endl;
+                }
+                else {
+                    m_fenceInfo.resource.logObj     = logObj;
+                    m_fenceInfo.state.logObjCreated = false;
+                }
+
+                if (logDeviceObj == nullptr) {
+                    LOG_ERROR (m_fenceInfo.resource.logObj) << NULL_DEPOBJ_MSG
+                                                            << std::endl;
+                    throw std::runtime_error (NULL_DEPOBJ_MSG);
+                }
+                m_fenceInfo.resource.logDeviceObj   = logDeviceObj;
+            }
+
+            void initFenceInfo (const VkFenceCreateFlags createFlags) {
+                m_fenceInfo.meta.createFlags = createFlags;
+                m_fenceInfo.resource.fence   = nullptr;
+            }
+
+            void waitForFence (void) {
+                vkWaitForFences (*m_fenceInfo.resource.logDeviceObj->getLogDevice(),
+                                  1,
+                                  &m_fenceInfo.resource.fence,
+                                  VK_TRUE,
+                                  UINT64_MAX);
+            }
+
+            /* Note that, fences must be reset manually to put them back into the unsignaled state. This is because
+             * fences are used to control the execution of the host, and so the host gets to decide when to reset the
+             * fence. Contrast this to semaphores which are used to order work on the GPU without the host being involved
+            */
+            void resetFence (void) {
+                vkResetFences (*m_fenceInfo.resource.logDeviceObj->getLogDevice(),
+                                1,
+                                &m_fenceInfo.resource.fence);
+            }
+
+            VkFence* getFence (void) {
+                return &m_fenceInfo.resource.fence;
+            }
+
+            void createFence (void) {
+                VkFenceCreateInfo createInfo;
+                createInfo.sType = VK_STRUCTURE_TYPE_FENCE_CREATE_INFO;
+                createInfo.pNext = nullptr;
+                createInfo.flags = m_fenceInfo.meta.createFlags;
+
+                auto result = vkCreateFence (*m_fenceInfo.resource.logDeviceObj->getLogDevice(),
+                                              &createInfo,
+                                              nullptr,
+                                              &m_fenceInfo.resource.fence);
+                if (result != VK_SUCCESS) {
+                    LOG_ERROR (m_fenceInfo.resource.logObj) << "[?] Fence"
+                                                            << " "
+                                                            << "[" << string_VkResult (result) << "]"
+                                                            << std::endl;
+                    throw std::runtime_error ("[?] Fence");
+                }
+                LOG_INFO (m_fenceInfo.resource.logObj)      << "[O] Fence"
+                                                            << std::endl;
+            }
+
+            void destroyFence (void) {
+                vkDestroyFence  (*m_fenceInfo.resource.logDeviceObj->getLogDevice(),
+                                  m_fenceInfo.resource.fence,
+                                  nullptr);
+                LOG_INFO (m_fenceInfo.resource.logObj) << "[X] Fence"
+                                                       << std::endl;
+            }
+
+            ~VKFence (void) {
+                if (m_fenceInfo.state.logObjCreated)
+                    delete m_fenceInfo.resource.logObj;
+            }
+    };
+}   // namespace Core
---
+++ Core/VKSemaphore.h
@@ -0,0 +1,112 @@
+#pragma once
+#define GLFW_INCLUDE_VULKAN
+#include <GLFW/glfw3.h>
+#include <vulkan/vk_enum_string_helper.h>
+#include "../Backend/Layer/LYInstanceBase.h"
+#include "../Backend/Log/LGImpl.h"
+#include "VKLogDevice.h"
+
+namespace Core {
+    class VKSemaphore: public Layer::LYInstanceBase {
+        private:
+            struct SemaphoreInfo {
+                struct Meta {
+                    VkSemaphoreCreateFlags createFlags;
+                } meta;
+
+                struct State {
+                    bool logObjCreated;
+                } state;
+
+                struct Resource {
+                    Log::LGImpl* logObj;
+                    VKLogDevice* logDeviceObj;
+                    VkSemaphore semaphore;
+                } resource;
+            } m_semaphoreInfo;
+
+        public:
+            VKSemaphore (Log::LGImpl* logObj,
+                         VKLogDevice* logDeviceObj) {
+
+                m_semaphoreInfo = {};
+
+                if (logObj == nullptr) {
+                    m_semaphoreInfo.resource.logObj     = new Log::LGImpl();
+                    m_semaphoreInfo.state.logObjCreated = true;
+
+                    m_semaphoreInfo.resource.logObj->initLogInfo ("Build/Log/Core", __FILE__);
+                    LOG_WARNING (m_semaphoreInfo.resource.logObj) << NULL_LOGOBJ_MSG
+                                                                  << std::endl;
+                }
+                else {
+                    m_semaphoreInfo.resource.logObj     = logObj;
+                    m_semaphoreInfo.state.logObjCreated = false;
+                }
+
+                if (logDeviceObj == nullptr) {
+                    LOG_ERROR (m_semaphoreInfo.resource.logObj) << NULL_DEPOBJ_MSG
+                                                                << std::endl;
+                    throw std::runtime_error (NULL_DEPOBJ_MSG);
+                }
+                m_semaphoreInfo.resource.logDeviceObj   = logDeviceObj;
+            }
+
+            void initSemaphoreInfo (const VkSemaphoreCreateFlags createFlags) {
+                m_semaphoreInfo.meta.createFlags   = createFlags;
+                m_semaphoreInfo.resource.semaphore = nullptr;
+            }
+
+            VkSemaphore* getSemaphore (void) {
+                return &m_semaphoreInfo.resource.semaphore;
+            }
+
+            /* A semaphore is used to add order between queue operations. Queue operations refer to the work we submit
+             * to a queue, either in a command buffer or from within a function. Semaphores are used both to order work
+             * inside the same queue and between different queues
+             *
+             * The way we use a semaphore to order queue operations is by providing the same semaphore as a 'signal'
+             * semaphore in one queue operation and as a 'wait' semaphore in another queue operation. For example, lets
+             * say we have semaphore S and queue operations A and B that we want to execute in order. What we tell Vulkan
+             * is that A will 'signal' semaphore S when it finishes executing, and B will 'wait' on semaphore S before
+             * it begins executing. When A finishes, semaphore S will be signaled, while B wont start until semaphore S
+             * is signaled. After B begins executing, semaphore S is automatically reset back to being unsignaled,
+             * allowing it to be used again
+             *
+             * Note that, the waiting only happens on the GPU. The CPU continues running without blocking
+            */
+            void createSemaphore (void) {
+                VkSemaphoreCreateInfo createInfo;
+                createInfo.sType = VK_STRUCTURE_TYPE_SEMAPHORE_CREATE_INFO;
+                createInfo.pNext = nullptr;
+                createInfo.flags = m_semaphoreInfo.meta.createFlags;
+
+                auto result = vkCreateSemaphore (*m_semaphoreInfo.resource.logDeviceObj->getLogDevice(),
+                                                  &createInfo,
+                                                  nullptr,
+                                                  &m_semaphoreInfo.resource.semaphore);
+                if (result != VK_SUCCESS) {
+                    LOG_ERROR (m_semaphoreInfo.resource.logObj) << "[?] Semaphore"
+                                                                << " "
+                                                                << "[" << string_VkResult (result) << "]"
+                                                                << std::endl;
+                    throw std::runtime_error ("[?] Semaphore");
+                }
+                LOG_INFO (m_semaphoreInfo.resource.logObj)      << "[O] Semaphore"
+                                                                << std::endl;
+            }
+
+            void destroySemaphore (void) {
+                vkDestroySemaphore  (*m_semaphoreInfo.resource.logDeviceObj->getLogDevice(),
+                                      m_semaphoreInfo.resource.semaphore,
+                                      nullptr);
+                LOG_INFO (m_semaphoreInfo.resource.logObj) << "[X] Semaphore"
+                                                           << std::endl;
+            }
+
+            ~VKSemaphore (void) {
+                if (m_semaphoreInfo.state.logObjCreated)
+                    delete m_semaphoreInfo.resource.logObj;
+            }
+    };
+}   // namespace Core
