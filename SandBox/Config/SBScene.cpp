1f0e1b28be7c7e81fad0bcc7eb56cee7c8bc4e7c
vijoy-sunil <vijoy.sunil@gmail.com>
2025-03-28 20:27:57 +0530

    minor refactoring

--- SandBox/Config/SBScene.cpp
+++ SandBox/Config/SBScene.cpp
@@ -1,3 +1,6 @@
+#define GLM_FORCE_RADIANS
+#define GLM_FORCE_DEPTH_ZERO_TO_ONE
+#define GLM_ENABLE_EXPERIMENTAL
 #include <glm/glm.hpp>
 #include <string>
 #include <vector>
--- SandBox/System/SYCameraController.h
+++ SandBox/System/SYCameraController.h
@@ -1,4 +1,7 @@
 #pragma once
+#define GLM_FORCE_RADIANS
+#define GLM_FORCE_DEPTH_ZERO_TO_ONE
+#define GLM_ENABLE_EXPERIMENTAL
 #include <glm/glm.hpp>
 #include <stdexcept>
 #include "../../Backend/Scene/SNSystemBase.h"
