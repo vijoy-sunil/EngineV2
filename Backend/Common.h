ceb4212ba1bc1d843471063c9146246e3bbe3591
vijoy-sunil <vijoy.sunil@gmail.com>
2025-09-11 18:00:39 +0530

    added new headers

--- Backend/Common.h
+++ Backend/Common.h
@@ -3,12 +3,24 @@
 #define GLFW_INCLUDE_VULKAN
 #include <GLFW/glfw3.h>
 #include <vulkan/vk_enum_string_helper.h>
+/* Icons */
+#include <IconFontCppHeaders/IconsFontAwesome6.h>
+/* Imgui */
+#include <imgui.h>
+#include <cpp/imgui_stdlib.h>
+#include <imgui_impl_glfw.h>
+#include <imgui_impl_vulkan.h>
+/* Implot */
+#include <implot.h>
+#include <implot_internal.h>
 /* GLM */
 #define GLM_FORCE_RADIANS
 #define GLM_FORCE_DEPTH_ZERO_TO_ONE
 #define GLM_ENABLE_EXPERIMENTAL
 #include <glm/glm.hpp>
 #include <glm/gtx/hash.hpp>
+#include <glm/gtx/euler_angles.hpp>
+#include <glm/gtc/type_ptr.hpp>
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtc/quaternion.hpp>
 /* Container */
@@ -32,7 +44,9 @@
 #include <algorithm>
 /* Utility */
 #include <chrono>
+#include <cstdio>
 #include <cstdint>
+#include <cfloat>
 #include <iomanip>
 #include <iterator>
 #include <stdexcept>
